117d228440422ba07a92095457e7b4950cb408a3
Jesse Songok <jessesongok@gmail.com>
2022-10-16 13:21:53 +0000

    added load products

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,1 +1,8 @@
 export const prerender = true;
+export const load = async({fetch}) => {
+    const res = await fetch('https://dummyjson.com/products')
+    const products = res.json()
+    return {
+        products
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
+	export let data;
+	let { products } = data;
+	console.log(products)
 </script>
 
 <svelte:head>
@@ -8,22 +10,11 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
 
 	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
+		Learn Sveltekit 
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
